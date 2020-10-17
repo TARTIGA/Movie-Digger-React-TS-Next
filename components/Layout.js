d2cdbe5731c1b2d7a35d3d1c503c749e598a5cfc
Tim <timigntrt@gmail.com>
2020-10-17 16:13:49 +0300

    add eslint and prettier

--- components/Layout.js
+++ components/Layout.js
@@ -1,11 +1,7 @@
-import Head from 'next/head';
 import {SITE_NAME} from '../constants'
 const Layout = () => {
   return (
     <div>
-      <Head>
-        <title>Blog App</title>
-      </Head>
       <h1>{SITE_NAME}</h1>
     </div>
   )
--- pages/index.js
+++ pages/index.js
@@ -1,22 +1,24 @@
-import { useState, useEffect } from 'react';
-import Head from 'next/head';
-import {SITE_NAME} from '../constants'
+import { useEffect } from "react"
+import Head from "next/head"
+import { SITE_NAME } from "../constants"
 const Home = () => {
+  useEffect(() => {
+    console.log(["test", test])
+  }, [])
   return (
     <div>
       <Head>
         <title>{SITE_NAME}</title>
-        <meta charset="utf-8" />
+        <meta charSet="utf-8" />
         <meta name="viewport" content="width=device-width, initial-scale=1" />
-         <meta name="theme-color" content="#000000" />
-         <meta name="description" content={SITE_NAME} />
-         <meta
-           name="viewport"
-           content="width=device-width, initial-scale=1, shrink-to-fit=no, user-scalable=no, viewport-fit=cover"
-       />
+        <meta name="description" content={SITE_NAME} />
+        <meta
+          name="viewport"
+          content="width=device-width, initial-scale=1, shrink-to-fit=no, user-scalable=no, viewport-fit=cover"
+        />
       </Head>
       <h1>{SITE_NAME}</h1>
     </div>
   )
 }
-export default Home;
+export default Home
